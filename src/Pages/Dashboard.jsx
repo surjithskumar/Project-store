932271cdb237b90bf02b4972e2f5763a3580beec
SURJITH S KUMAR <surjithskumar23@gmail.com>
2024-12-04 23:51:41 +0530

    complete ui

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -7,18 +7,18 @@ import Profile from '../Components/Profile'
 const Dashboard = () => {
   return (
     <>
-      <Header/>
-      <div className='container-fluid' style={{width:'100%',height:'90vh'}}>
+      <Header />
+      <div className='container-fluid' style={{ width: '100%', height: '90vh' }}>
         <Row>
           {/* My projects */}
           <Col sm={12} md={8}>
-          <h1>Welcome <span className='text-warning fw-bolder'>User</span></h1>
-          <MyProjects/>
+            <h1>Welcome <span className='text-warning fw-bolder'>User</span></h1>
+            <MyProjects />
           </Col>
-          
+
           {/* Profile */}
           <Col sm={12} md={4}>
-          <Profile/>
+            <Profile />
           </Col>
         </Row>
       </div>
